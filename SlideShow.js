ec90a9d6f2d5e673e79d7d544debe5a55c1cfbca
monica22schmidt <monica22schmidt@gmail.com>
2020-05-08 22:14:22 -0500

    javascript slide

--- SlideShow.js
+++ SlideShow.js
@@ -1,16 +1,28 @@
 var imageIndex = 0
-slides();
+showSlides(slideIndex);
 
-function slides(){
+function plusSlides(n){
+  showSlides(slideIndex +=n);
+}
+function currentSlide(n){
+  showSlides(slideIndex = n);
+}
+function showSlides(n){}
   var image;
-  var images = document.getElementsbyClassName("effects")
-  for (image = 0; image < images.length; image++){
-    images[image].style.display = none;
+  var images = document.getElementsbyClassName("slides")
+  var circles = document.getElementsbyClassName("circle")
+  if(n > slides.length){
+    slideIndex = 1;
   }
-  imageIndex++;
-  if (imageIndex > images.length) {
-    imageIndex = 1;
+  if(n < 1){
+    slideIndex = slides.length;
+  }
+  for (image = 0; image < images.length; image++){
+    images[image].style.display = "none";
+  }
+  for (circle = 0; circle < circles.length; circle++){
+    circles[circle].className = circles[circle].className.replace(" active", "");
   }
   images[imageIndex - 1].style.display = "block";
-  setTimeout(slides, 5000);
+  circles[slideIndex - 1].className += " active";
 }
